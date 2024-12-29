045176fbc8998bd6472771481c165178f299fb5d
Aman Sachan <codewithaman78@gmail.com>
2024-12-29 17:34:01 +0530

    Hero Section Done

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -6,9 +6,9 @@ import Button from "@/components/Button";
 const Header = () => {
   return (
     <>
-      <header className="py-4 border-b border-white/15 md:border-none">
+      <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
         <div className="container">
-          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+          <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
             <div>
               <div className="border border-white/15 rounded-lg h-10 w-10 inline-flex items-center justify-center">
                 <LogoIcon className="h-8 w-8" />
@@ -44,7 +44,7 @@ const Header = () => {
             </div>
             <div className="flex gap-4 items-center">
               {/* Button here */}
-              <Button>Join Waitlist</Button>
+              <Button>Join waitlist</Button>
               <MenuIcon className="md:hidden" />
             </div>
           </div>
