7acdf0edf8e5b352e975b8e0546281c48d70be06
Aman Sachan <codewithaman78@gmail.com>
2024-12-29 19:55:22 +0530

    Testimonials Section -- Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Features from "@/sections/Features";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import LogoTicker from "@/sections/LogoTicker";
+import Testimonials from "@/sections/Testimonials";
 
 
 export default function Home() {
@@ -10,7 +11,8 @@ export default function Home() {
       <Header />
       <Hero />
       <LogoTicker />
-      <Features/>
+      <Features />
+      <Testimonials/>
     </>
   );
 }
